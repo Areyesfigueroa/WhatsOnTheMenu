e89d960a616e324c465be3f2f5d26ee1187a6f87
Aliel <alielreyes@gmail.com>
2017-04-04 01:10:52 -0700

    Initial commit

---
+++ app/src/main/java/edu/afigueroacogswell/whatsonthemenu/MainActivity.java
@@ -0,0 +1,79 @@
+package edu.afigueroacogswell.whatsonthemenu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //References
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        /*
+        * Finds the toolbar object and sets it to be the new action bar not the default one.
+        * */
+        toolbar= (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        /*
+        * Inflate the menu; this adds items to the actions/tools bar if it is present
+        * */
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //Functions on the main file
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        /*
+        * Handle action bar item clicks here. The action bar will
+        * automatically handle clicks on te Home/Up button, so long
+        * as you specify a parent activity in AndroidManifest.xml
+        * */
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings)
+        {
+            Toast.makeText(this, "Hey you just hit" + item.getTitle(), Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (id == R.id.navigate)
+        {
+            startActivity(new Intent(this, SubActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
