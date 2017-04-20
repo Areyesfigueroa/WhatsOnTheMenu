b2214a681df9158245fd81c023bf4deb31d2e60e
Aliel <alielreyes@gmail.com>
2017-04-19 22:47:42 -0700

    Added class

---
+++ app/src/main/java/edu/afigueroacogswell/whatsonthemenu/Refrigerator.java
@@ -0,0 +1,23 @@
+package edu.afigueroacogswell.whatsonthemenu;
+
+/**
+ * Created by aliel on 4/19/2017.
+ */
+
+public class Refrigerator
+{
+    /*
+    * Encapsulates the Food's
+     * Data
+    * */
+    public class FoodItem
+    {
+        String name;
+        float portionSize;
+        String ounces;
+        int totalServings;
+    }
+}
+
+
+
